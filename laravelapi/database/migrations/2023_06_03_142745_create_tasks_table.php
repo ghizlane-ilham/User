8127841ed1de84a800e70b7e9623133d71e795d6
ghizlane-ilham <126577195+ghizlane-ilham@users.noreply.github.com>
2023-06-04 22:11:11 +0100

    Add files via upload

---
+++ laravelapi/database/migrations/2023_06_03_142745_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->string('startat');
+            $table->string('endat');
+            $table->boolean('isallday')->nullable()->default(false);
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
